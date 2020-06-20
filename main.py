67f06f02c7831ab038223ce5f8e804f5d3bfe2d8
Moritz Bergmann <mobergmann@pm.me>
2020-06-20 14:03:20 +0200

    added documentation and commented

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ from emoji import UNICODE_EMOJI
 import json
 import re
 
-pattern_command = re.compile("!(?:vote|poll) {([\w ]+)} ((?:\[[\w ]+\](?:{[^}]*})? ?){2,10})")
+pattern_command = re.compile(
+    "!(?:vote|poll) {([\w ]+)} ((?:\[[\w ]+\](?:{[^}]*})? ?){2,10})")
 pattern_option = re.compile("(\[[\w ]+\])({[^}]*})?")
-pattern_emoji = re.compile("")
 
 """default emojis"""
 predefined_emojis = [
@@ -30,47 +30,65 @@ predefined_emojis = [
     "🔟"
 ]
 
-predefined_status = [
-    "rig ballot papers",
-    "buying votes from china",
-    "buying votes from india",
-    "buying votes from russia",
-    "retuning!!!",
-    "calculating next outcome",
-    "preparing retuning",
-    "preparing next outcome",
-    "adding votes to win my bet",
-    "paying bribe",
-    "manipulate election",
-    "overthinking last bet"
-]
 
 def checkEmoji(emoji_to_check: str, position: int, server_emojis: list) -> str:
+    """Checks if the emoji is a valid emoji.
+    If not returns a default emoji, based on the current position.
+
+    Args:
+        emoji_to_check (str): The Emoji which should be validated.
+        position (int): The position of the current option. Used for replacement when the emoji is invalid.
+        server_emojis (list): A list of Emojis available on the guild from which the message has been send.
+
+    Returns:
+        str: The original emoji, if the Emoji is supported, or a default emoji based on the position of the option.
     """
-    checks if the emoji is a valid emoji. if not returns a default emoji, based on the current position.
-    TODO add doc
+
+    if emoji_to_check in UNICODE_EMOJI:  # if the given emoji is not in default emoji set
+        return emoji_to_check
+
+    # if the emoji is not available from the server
+    if isServerEmoji(emoji_to_check=emoji_to_check, server_emojis=server_emojis):
+        return emoji_to_check
+
+    return predefined_emojis[position]  # set the default emoji
+
+
+def isServerEmoji(emoji_to_check: str, server_emojis: list) -> tuple:
+    """Checks, if the Guild has the given emoji and if its available.
+
+    Args:
+        emoji_to_check (str): The emoji, which should be checked.
+        server_emojis (list): A list of all emojis available on the server.
+
+    Returns:
+        bool: True if the Guild has the emoji and its available. False otherwise. 
     """
 
-    if emoji_to_check in UNICODE_EMOJI: # if the given emoji is not in the default emoji set
-        return emoji_to_check
-        
-    if isServerEmoji(emoji_to_check, server_emojis): # if also not an custom server emoji
-        return emoji_to_check
-
-    return predefined_emojis[position] # set the default emoji
-    
-    
-
-def isServerEmoji(emoji_to_check: str, server_emojis: list) -> bool: 
     for server_emoji in server_emojis:
-        constructed_emoji = "<:" + server_emoji.name + ":" + str(server_emoji.id) + ">"
-        constructed_emoji_animated = "<a:" + server_emoji.name + ":" + str(server_emoji.id) + ">"
+        constructed_emoji = "<:" + server_emoji.name + ":" + \
+            str(server_emoji.id) + ">"  # generate the name of the emoji
+        constructed_emoji_animated = "<a:" + server_emoji.name + ":" + \
+            str(server_emoji.id) + \
+            ">"  # generate the name of the emoji if it was an animated emoji
+        # check if the emoji is on the guild
         if emoji_to_check == constructed_emoji or emoji_to_check == constructed_emoji_animated:
+            if not server_emoji.available:  # check if the emoji is available
+                return False
             return True
     return False
 
+
 def extractParams(input: str):
-    # TODO doc
+    """Extracts the title and the options of the message content with the help of regex. 
+    The title is of type string, and the options is of type list, containing tuples with name and emoji.
+
+    Args:
+        input (str): the message.content
+
+    Returns:
+        tuple: Returns a tuple, containing the title and a list of the options.
+    """
 
     tmp_list_1 = re.findall(pattern_command, input)
     match = tmp_list_1[0]
@@ -84,44 +102,57 @@ def extractParams(input: str):
 
     options = []
     for option in pre_options:
-        name = option[0][1 : len(option[0])-1]
-        emoji = option[1][1 : len(option[1])-1]
+        name = option[0][1: len(option[0])-1]
+        emoji = option[1][1: len(option[1])-1]
         options.append((name, emoji))
 
     return title, options
 
-def generateEmbed(title: str, author: discord.User, options: list):
-    # TODO doc
+
+def generateEmbed(title: str, author: discord.User, options: list) -> discord.Embed:
+    """Generate an embed for the vote and returns the embed.
+
+    Args:
+        title(str): the title of the Vote
+        author(discord.User): the User, which created the Vote
+        options(list): the list of options, which has to be embeded
+
+    Returns:
+        discord.Embed: Returns the generated embed
+    """
 
     embed = discord.Embed(
-        title = title,
+        title=title,
         # description = "desc",
-        timestamp = datetime.datetime.now())
+        timestamp=datetime.datetime.now())
     embed.set_author(
-        name=author.display_name, 
+        name=author.display_name,
         icon_url=author.avatar_url)
 
     for option in options:
-        embed.add_field(name="\u200b", value=option[1] + " - " + option[0], inline=False)
+        embed.add_field(
+            name="\u200b", value=option[1] + " - " + option[0], inline=False)
 
     return embed
 
 
-"""" get token"""
+"""" parse token from secrets.json"""
 with open("secrets.json") as json_data:
     token = json.load(json_data)["token"]
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    if not re.match(pattern_command, message.content): # command_pattern.match(message.content):
+    if not re.match(pattern_command, message.content):
         return
 
     await message.delete()
@@ -135,7 +166,8 @@ async def on_message(message):
     i = -1
     for option in options:
         i += 1
-        options[i] = (option[0], checkEmoji(emoji_to_check=option[1], position=i, server_emojis=message.guild.emojis))
+        options[i] = (option[0], checkEmoji(emoji_to_check=option[1],
+                                            position=i, server_emojis=message.guild.emojis))
 
     embed = generateEmbed(title=title, author=message.author, options=options)
 
@@ -143,7 +175,7 @@ async def on_message(message):
 
     for option in options:
         await send_message.add_reaction(option[1])
-        
+
     # TODO check message longer that 6000 characters
 
 client.run(token)
